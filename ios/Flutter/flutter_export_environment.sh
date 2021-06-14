a1df000be85d79769448bb1a0988d3d33dd37b6d
João Paulo Vieira <joaopaulovieira.dev@gmail.com>
2021-06-14 18:41:28 -0300

    Desafio Concluído

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\OneDrive\Ignite - Trilha Flutter\Desafios\Desafio 01 - Estrutura da aplicação\ignite_desafio_estrutura_da_aplicacao"
+export "FLUTTER_APPLICATION_PATH=D:\OneDrive\Ignite - Trilha Flutter\Desafios\Desafio 01 - Estrutura da aplicacao\ignite_desafio_estrutura_da_aplicacao"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
